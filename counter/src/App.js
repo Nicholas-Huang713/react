4f4f9d3b41af586b0ad7e8b42a197cb51d974df7
Nicholas_Huang713 <nhuang713@gmail.com>
2019-10-17 21:33:35 -0700

    Counter complete

---
+++ counter/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Counter from './Counter/Counter';
+
+function App() {
+  return (
+    <div className="container text-center">
+      <Counter name="Nick" />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ counter/src/Counter/Counter.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+
+class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            count: 0
+        }
+    }
+
+    updateCount() {
+        this.setState(prevState => ({
+            count: prevState.count + 1 
+        }));
+    }
+
+    render() {
+        let button;
+        if(this.state.count > 14){
+            button = <button className="btn btn-danger"
+                        onClick={ () => this.updateCount() }>
+                        Click Me        
+                    </button>
+        } else{
+            button = <button className="btn btn-primary"
+                        onClick={ () => this.updateCount() }>
+                        Click Me        
+                    </button>
+        }
+        
+        return (
+            <div className="jumbotron">
+                <h1>Hello {this.props.name}</h1>
+                {button}
+
+                
+                <h4>You clicked me {this.state.count} times</h4>
+            </div>
+        )
+    }
+}
+export default Counter;
